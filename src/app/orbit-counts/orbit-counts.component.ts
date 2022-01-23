fac5c1c8bf326196eba9b783c6d8b29f9893c89c
vy3191 <venkyafhs@gmail.com>
2022-01-23 18:50:27 -0500

    completed - Counting Satellites

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -6,26 +6,56 @@ import { Satellite } from '../satellite';
   templateUrl: './orbit-counts.component.html',
   styleUrls: ['./orbit-counts.component.css']
 })
+
 export class OrbitCountsComponent implements OnInit {
 
 	@Input() satellites: Satellite[];
 
-  constructor() { }
+    
+	constructor() { }
+  
+    myCount: number = 0;
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+	  this.getLength();
 
-  countByType(type: string): number {
-	let count = 0;
-	if (this.satellites) {
-	  for (let i = 0; i < this.satellites.length; i++) {
-		 if (this.satellites[i].type === type) {
-			count++;
-		 }
-	  }
+    }
+
+    getLength(): number {
+	  this.myCount = this.satellites.length
+	  return this.myCount
+    }
+
+    countByType(type: string): number {
+		console.log(this.satellites)
+		let count = 0;
+		if (this.satellites) {
+			for (let i = 0; i < this.satellites.length; i++) {
+				if (this.satellites[i].type === type) {
+					count++;
+				}
+			}
+	    }
+		return count;
 	}
-	return count;
- }
+
+	fetchNoDuplicates(): Satellite[] {
+		let seen: Object = {};
+		let count: number = 0;
+		let withOutDuplicates: Satellite[] = [];
+
+		for(let i: number = 0; i < this.satellites.length; i++) {
+			if(!seen[this.satellites[i].type]) {
+				console.log(this.satellites[i])
+				withOutDuplicates[count] = this.satellites[i];
+				count++;
+			}
+
+			seen[this.satellites[i].type] = true
+		}
+
+		return withOutDuplicates;
+	}
 
 
 }
